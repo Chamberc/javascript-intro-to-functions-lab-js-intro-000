275062ab07d83042fca6f253d1eee91a06a4fcb5
Chenai Chambers <chambers.t@hotmail.co.uk>
2020-07-01 18:36:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper (string) {
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase())
-    return "I can\'t hear you!";
+    { return "I can\'t hear you!";
 
-  else if (string.toUpperCase())
-    return "YES INDEED!";
+  } else if (string.toUpperCase())
+    {return "YES INDEED!";
 
-  else
+  } else
   return "I love you, too"
 }
